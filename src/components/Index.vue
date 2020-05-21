d5c4109d54aa6d9d57b9bc288bfb94bc13660f38
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-21 14:34:24 +0700

    Delete (local) Data

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -2,6 +2,9 @@
   <div class="index container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)"
+          >delete</i
+        >
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
@@ -37,29 +40,41 @@ export default {
       ],
     };
   },
+  methods: {
+    deleteSmoothie(id) {
+      this.smoothies = this.smoothies.filter((smoothie) => {
+        return smoothie.id !== id;
+      });
+    },
+  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style>
+<style lang="scss" scoped>
 .index {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   grid-gap: 30px;
   margin-top: 60px;
-}
-
-.index h2 {
-  font-size: 1.8em;
-  text-align: center;
-  margin-top: 0;
-}
-
-.index .ingredients {
-  margin: 0 auto;
-}
-
-.index .ingredients li {
-  display: inline-block;
+  h2 {
+    font-size: 1.8em;
+    text-align: center;
+    margin-top: 0;
+  }
+  .ingredients {
+    margin: 0 auto;
+    li {
+      display: inline-block;
+    }
+  }
+  .delete {
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    cursor: pointer;
+    color: #aaa;
+    font-size: 1.4em;
+  }
 }
 </style>
