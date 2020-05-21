6a51d256494a357fcdb7af797d9856bce9f6185f
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-22 02:07:18 +0700

    Passing props via Routes

---
+++ src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<h1>Chat {{ name }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Chat",
+	props: ["name"],
+	data() {
+		return {};
+	}
+};
+</script>
+
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,19 @@ import Welcome from "@/components/Welcome";
 Vue.use(Router);
 
 export default new Router({
+	mode: "history",
 	routes: [
 		{
 			path: "/",
 			name: "Welcome",
 			component: Welcome
+		},
+		{
+			path: "/chat",
+			name: "Chat",
+			component: () =>
+				import(/* webpackChunkName: "chat" */ "../components/Chat.vue"),
+			props: true
 		}
 	]
 });
