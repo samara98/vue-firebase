c090a8afe4afd3a92918a7649d5f261a2447d586
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-21 16:44:07 +0700

    AddSmoothie Component

---
+++ src/views/AddSmoothie.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="add-smoothie container">
+    <h2 class="center-align indigo-text">Add New Smoothie Recipe</h2>
+    <form action="POST" @submit.prevent="AddSmoothie">
+      <div class="field title">
+        <label for="" class="title">Smoothie Title:</label>
+        <input type="text" name="title" v-model="title" />
+      </div>
+      <div class="field add-ingredient">
+        <label for="" class="add-ingredient">Add an ingredient:</label>
+        <input type="text" name="add-ingredient" />
+      </div>
+      <div class="field center-align">
+        <button class="btn pink">Add Smoothie</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddSmoothie",
+  data() {
+    return {
+      title: null,
+    };
+  },
+  methods: {
+    AddSmoothie() {
+      console.log(this.title);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.add-smoothie {
+  margin-top: 50px;
+  padding: 20px;
+  max-width: 500px;
+  h2 {
+    font-size: 2em;
+    margin: 20px auto;
+  }
+  .field {
+    margin: 20px auto;
+  }
+}
+</style>
