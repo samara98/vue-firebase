a28b464d9a207a2b647a22275ca0ac0028b79e8f
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-22 17:22:14 +0700

    Creating a New Map

--- src/views/home/GMap.vue
+++ src/views/home/GMap.vue
@@ -1,16 +1,47 @@
 <template>
 	<div class="map">
-		<h2>Map</h2>
+		<div class="google-map" id="map"></div>
 	</div>
 </template>
 
 <script>
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
 export default {
 	name: "GMap",
 	data() {
-		return {};
+		return {
+			lat: 53,
+			lng: -2
+		};
+	},
+	methods: {
+		renderMap() {
+			const map = new window.google.maps.Map(document.getElementById("map"), {
+				center: { lat: 53, lng: -2 },
+				zoom: 6,
+				maxZoom: 15,
+				minZoom: 3,
+				streetViewControl: false
+			});
+		}
+	},
+	mounted() {
+		this.renderMap();
 	}
 };
 </script>
 
-<style></style>
+<style>
+.google-map {
+	width: 100%;
+	height: 100%;
+	margin: 0 auto;
+	background: white;
+	position: absolute;
+	top: 0;
+	left: 0;
+	z-index: -1;
+}
+</style>
