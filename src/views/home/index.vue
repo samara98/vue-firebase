8228a9f8ba117773c250ed6397c15282ed3fe165
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-22 15:40:59 +0700

    Map Component

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,17 @@
 <template>
 	<div class="home">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+		<g-map />
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import GMap from "./GMap.vue";
 
 export default {
 	name: "Home",
 	components: {
-		HelloWorld
+		GMap
 	}
 };
 </script>
