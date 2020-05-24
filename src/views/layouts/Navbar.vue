cdf5dfb08bd0dce7d4f75265d0f555d979f24d04
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-24 19:46:57 +0700

    Logging a User Out

--- src/views/layouts/Navbar.vue
+++ src/views/layouts/Navbar.vue
@@ -4,8 +4,9 @@
 			<div class="container">
 				<a href="#" class="brand-logo left">Geo Ninjas</a>
 				<ul class="right">
-					<li><a href="#">Signup</a></li>
+					<li><router-link :to="{ name: `Signup` }">Signup</router-link></li>
 					<li><a href="#">Login</a></li>
+					<li><a href="#" @click="logout">Logout</a></li>
 				</ul>
 			</div>
 		</nav>
@@ -13,10 +14,22 @@
 </template>
 
 <script>
+import firebase from "firebase/app";
+
 export default {
 	name: "Navbar",
 	data() {
 		return {};
+	},
+	methods: {
+		logout() {
+			firebase
+				.auth()
+				.signOut()
+				.then(() => {
+					this.$router.push({ name: "Signup" });
+				});
+		}
 	}
 };
 </script>
