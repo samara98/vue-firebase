5c100afb6125222a9297f3fd1b3fe24623d6ecb6
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-05-25 22:14:45 +0700

    Adding Comments

--- src/views/profile/ViewProfile.vue
+++ src/views/profile/ViewProfile.vue
@@ -2,22 +2,74 @@
 	<div class="view-profile container">
 		<div class="card" v-if="profile">
 			<h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
+			<ul class="comments collection">
+				<li>Comments</li>
+			</ul>
+			<form @submit.prevent="addComment">
+				<div class="field">
+					<label for="comment-input">Add a comment</label>
+					<input
+						type="text"
+						name="comment"
+						id="commnet-input"
+						v-model="newComment"
+					/>
+					<p v-if="feedback" class="red-text center">{{ feedback }}</p>
+				</div>
+			</form>
 		</div>
 	</div>
 </template>
 
 <script>
 import db from "../../api/firebase-init";
+import firebase from "firebase/app";
 
 export default {
 	name: "ViewProfile",
 	data() {
 		return {
-			profile: null
+			profile: null,
+			newComment: null,
+			feedback: null,
+			user: null
 		};
+	},
+	methods: {
+		addComment() {
+			if (this.newComment) {
+				this.feedback = null;
+				db.collection("comments-geo-ninjas")
+					.add({
+						to: this.$route.params.id,
+						from: this.user.id,
+						content: this.newComment,
+						time: Date.now()
+					})
+					.then(() => {
+						this.newComment = null;
+					});
+			} else {
+				this.feedback = "You must enter a comment to add it";
+			}
+		}
 	},
 	created() {
 		const dbUsers = db.collection("users-geo-ninjas");
+
+		// get current user
+		dbUsers
+			.where("user_id", "==", firebase.auth().currentUser.uid)
+			.get()
+			.then(snapshot => {
+				snapshot.forEach(doc => {
+					this.user = {
+						id: doc.id,
+						...doc.data()
+					};
+				});
+			});
+
 		dbUsers
 			.doc(this.$route.params.id)
 			.get()
